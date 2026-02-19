7a7a28108e405ea02d2eb82cfcaf257b1e201fe3
Ritesh kumar <sidharthrk7890@gmail.com>
2026-02-19 14:28:13 +0530

    Basic of c++ program

---
+++ BASIC_OF_C++/02_program.cpp
@@ -0,0 +1,13 @@
+// WAP to print Hello World , Welcome to C++
+
+#include<iostream>  // Include input-output library
+using namespace std;  // Use standard namespace
+
+int main(){      // Main function 
+
+    cout << "Hello World" << endl; // Print "Hello World" and move to next line
+    cout << "Welcome to C++";
+
+    return 0;    //program executed successfully
+
+}
